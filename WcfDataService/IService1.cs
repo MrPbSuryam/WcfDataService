9bfe03317bf5031a73d81b88ed0fcff7f50e9c99
mrpbsurya <44495653+mrpbsurya@users.noreply.github.com>
2021-04-29 21:54:03 +0530

    Add project files.

---
+++ WcfDataService/IService1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfDataService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        List<PersonInfo> GetPesrsonInfoData();
+    }
+
+
+    [DataContract]
+    public class PersonInfo
+    {
+        string firstName = string.Empty;
+        string lastName = string.Empty;
+        string email = string.Empty;
+        string gender = string.Empty;
+
+        [DataMember]
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        [DataMember]
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        [DataMember]
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        [DataMember]
+        public string Gender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }
+    }
+}
---
+++ WcfDataService/Service1.svc.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfDataService
+{
+    public class Service1 : IService1
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["TestDataConnectionString"].ConnectionString.ToString();
+        
+        public List<PersonInfo> GetPesrsonInfoData()
+        {
+            DataTable dttable = new DataTable();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand("select * from TestInfoData", conn))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    adapter.Fill(dttable);
+                    conn.Close();
+                }
+            }
+
+            List<PersonInfo> _data = new List<PersonInfo>();
+            for (int i = 0; i < dttable.Rows.Count; i++)
+            {
+                PersonInfo mydata = new PersonInfo();
+                mydata.FirstName = Convert.ToString(dttable.Rows[i]["FirstName"]);
+                mydata.LastName = Convert.ToString(dttable.Rows[i]["LastName"]);
+                mydata.Email = Convert.ToString(dttable.Rows[i]["Email"]);
+                mydata.Gender = Convert.ToString(dttable.Rows[i]["Gender"]);
+                _data.Add(mydata);
+            }
+
+            return _data;
+        }
+    }
+}
